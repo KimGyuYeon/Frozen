a7be0ac2442c7cf672b9c3736ebb10ccea7f2ddc
KimGyuYeon <97150145+KimGyuYeon@users.noreply.github.com>
2022-04-28 23:58:08 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // main.js
 $(function(){
+  
     // 1단계 메뉴에 마우스가 올라가면 depth2 class='on'
     // ... 마우스가 나가면 depth2 class on 제거
     $('#gnb > ul > li').hover(
@@ -10,7 +11,7 @@ $(function(){
         $(this).children().removeClass('on');
       }
     );
-  
+    
     // 패럴랙스 배경 스크롤
     $.stellar({
       horizontalScrolling: false,
